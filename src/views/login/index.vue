517bc302b6763aae61876394baa000d3c096cc55
coder-wff <wffchong@163.com>
2023-03-18 21:38:23 +0800

    chore: 🔨 集成pinia

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,4 +2,11 @@
 	<div class="login-container">login</div>
 </template>
 
-<script setup lang="ts" name="login"></script>
+<script setup lang="ts" name="login">
+import { useGlobalStore } from '@/stores'
+
+const globalStore = useGlobalStore()
+
+const token = globalStore.token
+console.log('token', token)
+</script>
