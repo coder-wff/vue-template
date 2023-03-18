3f4084074bf5a4a5b98273285f90f9777f5791db
coder-wff <wffchong@163.com>
2023-03-19 00:03:36 +0800

    chore: 🔨 封装axios

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,9 +4,25 @@
 
 <script setup lang="ts" name="login">
 import { useGlobalStore } from '@/stores'
+import { onMounted } from 'vue'
+import { loginApi } from '@/api/modules/login'
+import md5 from 'js-md5'
 
 const globalStore = useGlobalStore()
 
 const token = globalStore.token
 console.log('token', token)
+
+onMounted(() => {
+	loginApi({
+		username: 'admin',
+		password: md5('123456')
+	})
+		.then(res => {
+			console.log('res', res)
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,22 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 				'@': resolve(__dirname, './src')
 			}
 		},
+		server: {
+			// 服务器主机名，如果允许外部访问，可设置为 "0.0.0.0"
+			host: '0.0.0.0',
+			port: 5173,
+			open: true,
+			cors: true,
+			// 跨域代理配置
+			proxy: {
+				'/api': {
+					target: 'https://mock.mengxuegu.com/mock/629d727e6163854a32e8307e', // easymock
+					// target: 'https://www.fastmock.site/mock/f81e8333c1a9276214bcdbc170d9e0a0', // fastmock
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api/, '')
+				}
+			}
+		},
 		plugins: [
 			vue(),
 			// element-plus 按需导入
