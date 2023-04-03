7e95dd884ba7e82df99800287fc0102a017caa42
wff <283953455@qq.com>
2023-04-03 17:35:18 +0800

    feat: 🚀 添加一些预定样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import icons from '@/global/registerIcons'
 import App from './App.vue'
+// reset style sheet
+import '@/styles/reset.scss'
+// CSS common style sheet
+import '@/styles/common.scss'
+// element dark(内置暗黑模式)
+import 'element-plus/theme-chalk/dark/css-vars.css'
+// custom element dark(自定义暗黑模式)
+import '@/styles/theme/element-dark.scss'
+// custom element css
+import '@/styles/element.scss'
 import router from '@/routers'
 // pinia store
 import pinia from '@/stores/index'
